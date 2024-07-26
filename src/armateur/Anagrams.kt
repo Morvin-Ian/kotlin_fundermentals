19c3abc7f4ef0243be553b47eb55922ed8fff1cf
OluochIan <morvinian@gmail.com>
2024-07-26 19:06:26 +0300

    anagram

--- src/armateur/Anagrams.kt
+++ src/armateur/Anagrams.kt
@@ -5,3 +5,9 @@ fun isAnagram(string1:String, string2: String):Boolean {
     val str2Group = string2.lowercase().filter { it.isLetter() }.groupBy { it }
     return str1Group == str2Group
 }
+
+fun isAnagram2(string1:String, string2: String):Boolean {
+    val str1Group = string1.toCharArray().sorted().joinToString("")
+    val str2Group = string2.toCharArray().sorted().joinToString("")
+    return str1Group == str2Group
+}
